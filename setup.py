e21d310080dba03cc8bb527ca6d0943c15322d21
Aina Siddiqui <aina_1235@hotmail.co.uk>
2024-11-04 01:55:00 +0500

    requirements & installations done

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md","r",encoding="utf-8")as f:
+    long_description = f.read()
+__version__='0.0.0'
+REPO_NAME="Text-Summarizer"
+AUTHOR_USER_NAME="Aina-Siddiqui"
+SRC_REP0="textSummarizer"
+AUTHOR_EMAIL="aina_1235@hotmail.co.uk"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python library for summarizing text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"}, 
+    packages=setuptools.find_packages(where="src")
+)
