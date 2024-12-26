b70f20a90b347e7150c80f65e1afe4fb2630bea7
Aina Siddiqui <aina_1235@hotmail.co.uk>
2024-12-27 01:21:04 +0500

    data ingestion added

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass,field
+from pathlib import Path
+from typing import List
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir:Path
+    source_URL:str
+    local_data_file:Path
+    unzip_dir:Path
+    
