0742eb77281db7ebea5f904a1d8e65913fb64b11
Aina Siddiqui <aina_1235@hotmail.co.uk>
2024-12-27 14:21:14 +0500

    data transformation notebook added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from src.textSummarizer.logging import logger
 
 STAGE_NAME='Data Ingestion stage'
@@ -16,6 +17,15 @@ try:
     logger.info(f">>>>>Starting stage {STAGE_NAME}>>>>>")
     data_validation= DataValidationPipeline()
     data_validation.main()
+    logger.info(f">>>>>>Completed stage {STAGE_NAME}<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e   
+STAGE_NAME='Data Transformation stage'
+try:
+    logger.info(f">>>>>Starting stage {STAGE_NAME}>>>>>")
+    data_transformation= DataTransformationPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>Completed stage {STAGE_NAME}<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
