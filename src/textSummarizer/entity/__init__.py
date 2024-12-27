affdeef94d129d99b3c7747152d058eca8b20196
Aina Siddiqui <aina_1235@hotmail.co.uk>
2024-12-27 13:47:24 +0500

    data validation notebook added

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,10 +1,17 @@
 from dataclasses import dataclass,field
 from pathlib import Path
 from typing import List
+
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir:Path
     source_URL:str
     local_data_file:Path
     unzip_dir:Path
-    
+
+
+@dataclass(frozen=True)
+class DataValidationConfig: 
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: List[str] = field(default_factory=list)  # Correctly defined with field
